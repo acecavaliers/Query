e4417d3a56ea83ee750025e3516cbc71ed307741
acecavaliers <aceofkagaw@gmail.com>
2024-03-16 16:57:42 +0800

    Updt proll

--- UPDT P-ROLL.sql
+++ UPDT P-ROLL.sql
@@ -2,20 +2,20 @@
 
 DECLARE @EMPID NVARCHAR(50), @PERIODFROM DATE , @AMOUNT MONEY --, @DESC NVARCHAR(200)
 
-SET @EMPID='22-17-2068'
-SET @PERIODFROM='2024-02-26'
-SET @AMOUNT = (select DAllowance from tbl_EmployeeInformation where EmpID = @EMPID)
+SET @EMPID='42-24-5104'
+SET @PERIODFROM='2024-03-04'
+-- SET @AMOUNT = (select DAllowance from tbl_EmployeeInformation where EmpID = @EMPID)
 -- SET @DESC = 'Allowance=' + CAST(@AMOUNT as nvarchar(100))
 
-UPDATE tbl_Payroll SET SOthers=SOthers+250,Holiday=RatePD*0.3 WHERE PeriodFrom=@PERIODFROM AND EmpID=@EMPID
+UPDATE tbl_Payroll SET RatePD=403 WHERE PeriodFrom=@PERIODFROM AND EmpID=@EMPID
 
--- UPDATE tbl_OtherDetails SET OSDet= REPLACE(OSDet,'423.115',@AMOUNT) WHERE DFrom=@PERIODFROM AND EmpID=@EMPID
-SET @AMOUNT = (select sothers from tbl_Payroll WHERE PeriodFrom=@PERIODFROM AND EmpID=@EMPID)
+-- UPDATE tbl_OtherDetails SET OSDet= 'Overpayment = 0.50' WHERE DFrom=@PERIODFROM AND EmpID=@EMPID
+-- SET @AMOUNT = (select sothers from tbl_Payroll WHERE PeriodFrom=@PERIODFROM AND EmpID=@EMPID)
 
-UPDATE tbl_OtherDetails SET OSDet= iif(@AMOUNT>0 ,CONCAT('Allowance=',@AMOUNT),''), HolDet='02/27/2024 w/ 30%' WHERE DFrom=@PERIODFROM AND EmpID=@EMPID
+-- UPDATE tbl_OtherDetails SET OSDet= iif(@AMOUNT>0 ,CONCAT('Allowance=',@AMOUNT),''), HolDet='02/27/2024 w/ 30%' WHERE DFrom=@PERIODFROM AND EmpID=@EMPID
 
 EXEC REGEN_PAYROLL_SELECTED @EMPID,@PERIODFROM
 
-select NOD,Holiday,TSalary,Net, RatePD from tbl_Payroll 
-where PeriodFrom ='2024-02-26' 
-and EmpID in ('06-22-3994','01-23-4312','01-10-0281')
+-- select RatePD ,* from tbl_Payroll 
+-- where PeriodFrom ='2024-03-04' 
+-- and EmpID in ('42-24-5104')
